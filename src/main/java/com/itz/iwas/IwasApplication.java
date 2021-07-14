640aa9364bb2bc07b191d55ed42f0f5825df77ef
abdul <itzabdulbaasit@gmail.com>
2021-07-14 16:19:43 +0530

    first commit

---
+++ src/main/java/com/itz/iwas/IwasApplication.java
@@ -0,0 +1,13 @@
+package com.itz.iwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwasApplication.class, args);
+	}
+
+}
